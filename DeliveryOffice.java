f8715511d0b8d9cbf47b4ae74cefdee5225f8719
Rodolfo Saul Islas Gonzalez <rodsislas@hotmail.com>
2012-11-19 20:33:50 -0600

    Second commit
    Second commit

--- DeliveryOffice.java
+++ DeliveryOffice.java
@@ -4,31 +4,6 @@ import java.util.Random;
 public class DeliveryOffice{
 	/*Class which will represent the different delivery offices*/
 
-	/*Variable which will hold the name of the office
-	public String nameOfOffice;
-	/*Variable used for the price for the delivery
-	private float priceForDelivery;
-	/*Variable to hold the days it will take the package to arrive
-	private int daysForDelivery;
-	/*Variables to position the office somewhere in a "plane"
-	private int xValue,yValue;
-
-	public DeliveryOffice(String nameOfOffice,int xValue,int yValue){
-		this.nameOfOffice = nameOfOffice;
-		this.xValue = xValue;
-		this.yValue = yValue;
-	}
-
-	public double calculateDistance(Client client){
-		//return Math.sqrt((Math.pow(((float)this.xValue - (float)client.getXValue()),2) + Math.pow(((float)this.yValue - (float)client.getYValue()),2)));
-		return Math.sqrt((float)Math.pow((this.xValue - client.getXValue()),2));
-	}
-
-	public String getNameOfOffice(){
-		return this.nameOfOffice;
-	}
-	*/
-
 	private String nameOfOffice;
 	private int xValue,yValue;
 	private Random random = new Random();
